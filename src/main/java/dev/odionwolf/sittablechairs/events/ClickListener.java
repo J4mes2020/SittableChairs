ba6791d2fae42ba3e3a542d9ee136bd35d08edab
PupJoey <james.lawrence@live.co.uk>
2021-03-20 22:30:38 +0000

    Initial Commit

--- src/main/java/dev/odionwolf/sittablechairs/events/ClickListener.java
+++ src/main/java/dev/odionwolf/sittablechairs/events/ClickListener.java
@@ -2,14 +2,24 @@ package dev.odionwolf.sittablechairs.events;
 
 import dev.odionwolf.sittablechairs.SittableChairs;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.vehicle.VehicleExitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class ClickListener implements Listener {
+
+    ArrayList<UUID> mobID = new ArrayList<>();
 
     public ClickListener(SittableChairs sittableChairs) {
         Bukkit.getPluginManager().registerEvents(this, sittableChairs);
@@ -19,6 +29,7 @@ public class ClickListener implements Listener {
     public void onClick(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         Block block = event.getClickedBlock();
+        World world = player.getWorld();
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             assert block != null;
             switch (block.getType()) {
@@ -58,8 +69,29 @@ public class ClickListener implements Listener {
                 case RED_NETHER_BRICK_STAIRS:
                 case RED_SANDSTONE_STAIRS:
                 case WARPED_STAIRS:
-                    player.sendMessage("You clicked on" + block);
+                    Location location = block.getLocation();
+                    LivingEntity entity = (LivingEntity) world.spawnEntity(location, EntityType.BAT);
+                    entity.addPassenger(player);
+                    entity.setAI(false);
+                    entity.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1000000, 100, false, false));
+                    entity.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 1000000, 100, false, false));
+                    entity.setSilent(true);
+                    mobID.add(entity.getUniqueId());
+                    player.sendMessage("You clicked on" + " " + block.getType());
+                    player.sendMessage(String.valueOf(mobID));
             }
+        }
+    }
+
+    @EventHandler
+    public void onDismount(VehicleExitEvent event) { // Doesn't fire
+        Player player = (Player) event.getExited();
+        System.out.println("exited");
+        player.sendMessage("exitied");
+        Vehicle en = event.getVehicle();
+        if (mobID.contains(en.getUniqueId())) {
+            mobID.remove(en.getUniqueId());
+            en.remove();
         }
     }
 }
